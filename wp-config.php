8c404429195909fa4050a21f0e3f991c2fb2fe39
Masri Manas <saya@masrimanas.com>
2021-01-06 17:42:24 +0700

    fixing db host

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'yq9ywg7mk3qk45mn' );
 define( 'DB_PASSWORD', 'yfji2fczs8aqc39i' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	' );
+define( 'DB_HOST', 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
