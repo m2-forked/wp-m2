cf5eb9dc234a582ba35c7758faf47b963f8ca16f
Masri Manas <saya@masrimanas.com>
2021-01-06 18:23:15 +0700

    modify wp config

--- wp-config2.php
+++ wp-config2.php
@@ -18,18 +18,22 @@
  * @package WordPress
  */
 
+//define('WP_HOME','http://apps.masrimanas.com');
+
+//define('WP_SITEURL','http://apps.masrimanas.com');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'hu5lzs16lnin8i99' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'yq9ywg7mk3qk45mn' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'yfji2fczs8aqc39i' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -77,9 +81,10 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy publishing. */
+define(‘COOKIE_DOMAIN’, $_SERVER[‘HTTP_HOST’] );
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
